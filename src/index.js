ffe686ca65962b8ade22a14fb49b2fc7c9cab0fc
dno-luigi <dno@ai-n.org>
2025-07-24 19:42:36 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import './index.css'; // <--- THIS LINE IS CRUCIAL
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+// ... rest of the file
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
